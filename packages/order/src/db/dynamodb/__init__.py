6b14c599dd917000ae92f60b7c61f59edcdb44fa
kokoaj <kokiebisu@icloud.com>
2022-08-18 21:46:21 -0700

    setup basic file structure

---
+++ packages/order/src/db/dynamodb/__init__.py
@@ -0,0 +1,2 @@
+class DynamoDB:
+    _db = None
---
+++ packages/order/src/services/__init__.py
@@ -0,0 +1,9 @@
+class OrderService:
+    def create_order(self):
+        '''
+        Preconditions:
+        UserService: Checks if the user exists and can place an orderand obtain their payment information
+        ProductService: Check if the product is still available
+        PaymentService: Authorize and fullfil payment
+        '''        
+        pass
