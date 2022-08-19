92566ddcf825129616ce78c4cf389b3031647903
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-08-19 15:25:42 -0700

    Add DynamoDB to order service (#11)
    * setup dependencies
    
    * setup repository
    
    * setup dynamodb

--- packages/order/src/db/dynamodb/__init__.py
+++ packages/order/src/db/dynamodb/__init__.py
@@ -1,2 +1,14 @@
-class DynamoDB:
+import boto3
+
+class DynamoDBDatabase:
+    _resource = None
     _db = None
+
+    def get_db(self):
+        if not self._resource:
+            self._resource = boto3.resource('dynamodb')
+        if not self._db:
+            self._db = self._resource.Table('orders')
+        return self._db
+
+    
