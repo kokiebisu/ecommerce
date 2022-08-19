29cd652e034658f849516823a9a8b01df6eded5a
kokoaj <kokiebisu@icloud.com>
2022-08-19 15:19:29 -0700

    setup dynamodb class

--- packages/order/src/repository/__init__.py
+++ packages/order/src/repository/__init__.py
@@ -1,12 +1,44 @@
+from uuid import uuid4
+
+from db.dynamodb import DynamoDBDatabase
+
+
 class OrderRepository:
-    def create_order(self):
-        pass
+    _db = DynamoDBDatabase.get_db()
 
-    def retrieve_order(self):
-        pass
+    @classmethod
+    def create(cls, user, quantity, product):
+        if not cls._db:
+            raise Exception('DB was not properly fetched')
+        cls._db.put_item(
+            Item={
+                'order_id': uuid4(),
+                'quantity': quantity,
+                'user_id': user['id'],
+                'first_name': user['first_name'],
+                'last_name': user['last_name'],
+                'product_id': product['id'],
+                'product_name': product['name']
+            }
+        )
 
-    def update_order(self):
-        pass
+    @classmethod
+    def retrieve(cls, order_id: str):
+        if not cls._db:
+            raise Exception('DB was not properly fetched')
+        response = cls._db.get_item(
+            Key={
+                'order_id': order_id
+            }
+        )
+        return response['Item']
 
-    def delete_order(self):
-        pass
+    @classmethod
+    def delete(cls, order_id):
+        if not cls._db:
+            raise Exception('DB was not properly fetched')
+        cls._db.delete_item(
+            Key={
+                'order_id': order_id
+            }
+        )
