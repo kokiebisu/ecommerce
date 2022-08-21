636b7ed90215c81510022bb3cfe35a95b7efe244
kokoaj <kokiebisu@icloud.com>
2022-08-21 14:47:09 -0700

    setup load balancer

---
+++ terraform/modules/load-balancer/security-group.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "load_balancer" {
+  name        = "${var.namespace}-lb-sg"
+  vpc_id      = var.vpc.id
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-lb-sg"
+  }
+}
