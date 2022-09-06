f3cac3101ce39fb9eec303d47bebc5a038547c36
kokoaj <43525282+kokiebisu@users.noreply.github.com>
2022-09-05 19:00:47 -0700

    UI Improvement (#27)
    * update webpack to allow svg loading
    
    * update web pack to enable loading .env files
    
    * seperate css and styles folders
    
    * update code

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,57 +1,81 @@
 const path = require("path");
+const dotenv = require("dotenv");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { DefinePlugin } = require("webpack");
 
 const dev = process.env.NODE_ENV !== "production";
 
-module.exports = {
-  entry: "./src/index.tsx",
-  target: "web",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: "babel-loader" },
-          {
-            loader: "@linaria/webpack-loader",
-            options: { sourceMap: dev },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-            options: { sourceMap: dev },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif|woff|woff2|eot|ttf|svg)$/,
-        use: [{ loader: "file-loader" }],
-      },
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((acc, cur) => {
+    acc[`process.env.${cur}`] = JSON.stringify(env[cur]);
+    return acc;
+  }, {});
+
+  return {
+    entry: "./src/index.tsx",
+    target: "web",
+    mode: "development",
+    resolve: {
+      modules: ["node_modules"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: "babel-loader" },
+            {
+              loader: "@linaria/webpack-loader",
+              options: { sourceMap: dev },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(svg)$/,
+          use: [
+            {
+              loader: "svg-url-loader",
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "index.bundle.js",
+      path: path.resolve(__dirname + "/dist"),
+    },
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      static: path.resolve(__dirname, "src/assets"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
+      new DefinePlugin(envKeys),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
-  ],
+  };
 };
